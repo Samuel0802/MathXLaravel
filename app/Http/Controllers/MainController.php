6f4f0c6dcb5ebe605b099e125ea98efd71dda80f
Samuel <samuelsouza0802@gmail.com>
2025-06-04 21:58:20 -0300

    Gerar Coleção de Exercícios

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -18,7 +18,7 @@ class MainController extends Controller
         'check_subtraction' => 'required_without_all::check_sum,check_multiplication,check_division',
         'check_multiplication' => 'required_without_all::check_sum,check_subtraction,check_division',
         'check_division' => 'required_without_all::check_sum,check_subtraction,check_multiplication',
-        'number_one' => 'required|integer|min:0|max:999',
+        'number_one' => 'required|integer|min:0|max:999|lt:number_two',
         'number_two' => 'required|integer|min:0|max:999',
         'number_exercises' => 'required|integer|min:5|max:50',
 
@@ -28,11 +28,95 @@ class MainController extends Controller
 
         ];
 
-
-
         $request->validate($regra, $feedback);
 
-          dd($request->all());
+        //identificar quais operações matemáticas o usuário escolheu em um formulário (via checkbox)
+        $operations = [];
+
+        if($request->check_sum){
+            $operations[] = 'sum';
+        }
+
+        if($request->check_subtraction){
+            $operations[] = 'subtraction';
+        }
+        if($request->check_multiplication){
+            $operations[] = 'multiplication';
+        }
+
+        if($request->check_division){
+            $operations[] = 'division';
+        }
+
+
+
+        //numeros (min e max)
+        $min = $request->number_one;
+        $max = $request->number_two;
+
+        //quantidade de exercícios
+        $numberExercises = $request->number_exercises;
+
+        //gerar exercicios
+        $exercises = [];
+
+        for($i = 1; $i <= $numberExercises; $i++){
+
+            //array_rand: Sortear todas as operações matemáticas
+            $operation = $operations[array_rand($operations)];
+
+            //rand: sortear valor min e max
+            $number1 = rand($min, $max);
+            $number2 = rand($min, $max);
+
+            $exercise = '';
+            $sollution = '';
+
+            switch($operation){
+               case 'sum':
+                 $exercise = "$number1 + $number2 = ";
+                 $sollution = $number1 + $number2;
+                 break;
+
+                 case 'subtraction':
+                 $exercise = "$number1 - $number2 = ";
+                 $sollution = $number1 - $number2;
+                 break;
+
+
+                 case 'multiplication':
+                 $exercise = "$number1 x $number2 = ";
+                 $sollution = $number1 * $number2;
+                 break;
+
+                 case 'division':
+
+                     //evitar que divisão por zero pode acontecer
+                     if($number2 == 0){
+                        $number2 = 1;
+                     }
+
+                 $exercise = "$number1 : $number2 = ";
+                 $sollution = $number1 / $number2;
+                 break;
+            }
+
+            //Adicionando 2 casa decimal na solução
+            if(is_float($sollution)){
+                $sollution = round($sollution, 2);
+            }
+
+             $exercises[] = [
+                'operation' => $operation,
+                'exercise_number' => $i,
+                'exercise' => $exercise,
+                'sollution' => "$exercise $sollution"
+             ];
+
+        }
+
+        dd($exercises);
+
     }
 
     public function printExercicios(){
